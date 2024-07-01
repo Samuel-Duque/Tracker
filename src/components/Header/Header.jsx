739b87e1245fcd34208f84460b3db1ae03802068
Samuel L P Duque <samuellocatel@gmail.com>
2024-06-30 22:29:37 -0300

    SearchBar in progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react"; // Importe useState
+import React, { useState } from "react";
 import style from "./Header.module.css";
 import downArrow from "../../assets/down-arrow.svg";
 import plusIcon from "../../assets/more-icon.svg";
 import searchIcon from "../../assets/search-icon.svg";
+import cancelIcon from "../../assets/cancel-icon.svg";
 
 function Header() {
-  // Estado para controlar a visibilidade da barra de pesquisa
   const [isSearchActive, setIsSearchActive] = useState(false);
 
-  // Função para alternar a visibilidade da barra de pesquisa
-  const toggleSearch = () => setIsSearchActive(!isSearchActive);
+  const toggleSearch = () => {
+    setIsSearchActive(!isSearchActive);
+    const searchBar = document.querySelector(`.${style.searchBar}`);
+    const btnLog = document.querySelector(`.${style.btnLog}`);
+    const searchIcon = document.querySelector(`.${style.searchIconSvg}`);
+
+    if (isSearchActive) {
+      searchBar.classList.add(`${style.searchBarVisible}`);
+
+      searchBar.style.display = "flex";
+      btnLog.style.display = "none";
+      searchIcon.style.display = "none";
+    } else {
+      searchBar.classList.remove(`${style.searchBarVisible}`);
+
+      searchBar.style.display = "none";
+      btnLog.style.display = "flex";
+      searchIcon.style.display = "flex";
+    }
+  };
 
   return (
     <>
@@ -25,30 +43,24 @@ function Header() {
             <span className={style.username}>Zythee</span>
             <img src={downArrow} alt="" />
           </div>
-
           <a href="/">Songs</a>
           <a href="/">Lists</a>
           <a href="/">Artists</a>
           <a href="/">Trackers</a>
-          {/* Altere para renderizar condicionalmente a barra de pesquisa ou o ícone de pesquisa */}
-          {isSearchActive ? (
-            <fieldset className={style.searchBar}>
-              <a href="#" onClick={toggleSearch}>
-                <img src={searchIcon} alt="" className={style.searchIconSvg} />
-              </a>
-              <input type="text" placeholder="" className={style.searchInput} />
-            </fieldset>
-          ) : (
+
+          <fieldset className={style.searchBar}>
             <a href="#" onClick={toggleSearch}>
-              <img src={searchIcon} alt="" className={style.searchIconSvg} />
+              <img src={cancelIcon} alt="" className={style.cancelIconSvg} />
             </a>
-          )}
-          {/* Renderize condicionalmente o botão + Log baseado no estado isSearchActive */}
-          {!isSearchActive && (
-            <div className={style.btnLog}>
-              <img src={plusIcon} alt="" /> Log
-            </div>
-          )}
+            <input type="text" placeholder="" className={style.searchInput} />
+          </fieldset>
+          <a href="#" onClick={toggleSearch}>
+            <img src={searchIcon} alt="" className={style.searchIconSvg} />
+          </a>
+          <a href="/" className={style.btnLog}>
+            <img src={plusIcon} alt="" />
+            Log
+          </a>
         </div>
       </div>
     </>
