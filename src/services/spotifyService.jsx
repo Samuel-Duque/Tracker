4cdfc8e30d6d2df59edef621179b02317742b87b
Vinicius <vinciuscarmo95@gmail.com>
2024-07-03 17:21:22 -0300

    spotify token update

--- src/services/spotifyService.jsx
+++ src/services/spotifyService.jsx
@@ -1,23 +1,37 @@
 // src/services/spotifyService.js
 import axios from "axios";
-
 const BASE_URL = "https://api.spotify.com/v1/";
-const TOKEN_URL =
-  "BQCRYh5wiqJikjAtrdBXynaEqX6IkJsRjMxbebLLNmzLtrZRXeOzOR-yH2OPqtRjnrR_UMid-PhJwXm9xfG-lj6_kHMabE1qimVYsVqBdhKu4DItBQCqOql_ZtGJmweI9quK2HgtAP3_ZcA5K5ZL3SwLapYA83cgD5-ErYys4JdVjCvxxPXmKpYfJSVJ4rMWiE5GXyL3jD_GG-LRGt7Y20gSXuEIWaNWZcsr9wbeVWcd-UX3aoXFcNLIfSoVxT45tvVWJJ2srTmbpPQynBolxcWAGTiLO_iO22weMh_Nf0Kg7vAXhlRmFVlmiESaNPyudoXiA5M";
 
-const getToken = async () => {
+const getSpotifyToken = async () => {
+  const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+  const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+  const authString = `${client_id}:${client_secret}`;
+  const base64AuthString = btoa(authString);
+
   try {
-    const response = await axios.get(TOKEN_URL);
-    return response.data.accessToken;
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${base64AuthString}`,
+      },
+      body: "grant_type=client_credentials",
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.access_token;
   } catch (error) {
-    console.error("Erro ao obter token:", error);
-    throw error;
+    console.error("Error fetching data: ", error);
   }
 };
 
 export const fetchArtistImage = async (artistName) => {
   try {
-    const token = import.meta.env.VITE_SPOTIFY_TOKEN;
+    const token = await getSpotifyToken();
     const response = await axios.get(`${BASE_URL}search`, {
       params: {
         type: "track",
@@ -33,7 +47,6 @@ export const fetchArtistImage = async (artistName) => {
     });
 
     const artist = response.data.tracks.items[0];
-    console.log(artist);
     if (artist && artist.album.images.length > 0) {
       return artist.album.images[0].url;
     } else {
