3dddf42dbb3e64f26a82147be6b12c785e49ffc8
Vinicius <vinciuscarmo95@gmail.com>
2024-07-09 16:40:16 -0300

    Update LastFmServices.jsx

--- src/services/LastFmServices.jsx
+++ src/services/LastFmServices.jsx
@@ -1,42 +1,42 @@
-// src/services/lastFmService.js
-import axios from "axios";
+// // src/services/lastFmService.js
+// import axios from "axios";
 
-const API_KEY = "3225eb353211538a3f2c24b66102bf0d";
-const BASE_URL = "http://ws.audioscrobbler.com/2.0/";
+// const API_KEY = "3225eb353...";
+// const BASE_URL = "http://ws.audioscrobbler.com/2.0/";
 
-const api = axios.create({
-  baseURL: BASE_URL,
-  params: {
-    api_key: API_KEY,
-    format: "json",
-  },
-});
+// const api = axios.create({
+//   baseURL: BASE_URL,
+//   params: {
+//     api_key: API_KEY,
+//     format: "json",
+//   },
+// });
 
-export const fetchTopArtists = async () => {
-  try {
-    const response = await api.get("", {
-      params: {
-        method: "chart.gettopartists",
-      },
-    });
-    return response.data.artists.artist.slice(0, 10);
-  } catch (error) {
-    console.error("Erro ao buscar top artistas:", error);
-    throw error;
-  }
-};
+// export const fetchTopArtists = async () => {
+//   try {
+//     const response = await api.get("", {
+//       params: {
+//         method: "chart.gettopartists",
+//       },
+//     });
+//     return response.data.artists.artist.slice(0, 10);
+//   } catch (error) {
+//     console.error("Erro ao buscar top artistas:", error);
+//     throw error;
+//   }
+// };
 
-export const fetchArtistInfo = async (artistName) => {
-  try {
-    const response = await api.get("", {
-      params: {
-        method: "artist.getinfo",
-        artist: artistName,
-      },
-    });
-    return response.data.artist;
-  } catch (error) {
-    console.error("Erro ao buscar informações do artista:", error);
-    throw error;
-  }
-};
+// export const fetchArtistInfo = async (artistName) => {
+//   try {
+//     const response = await api.get("", {
+//       params: {
+//         method: "artist.getinfo",
+//         artist: artistName,
+//       },
+//     });
+//     return response.data.artist;
+//   } catch (error) {
+//     console.error("Erro ao buscar informações do artista:", error);
+//     throw error;
+//   }
+// };
