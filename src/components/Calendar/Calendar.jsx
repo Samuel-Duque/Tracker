04716507777ab641d0da118d80e704eeca0f15e9
Vinicius <vinciuscarmo95@gmail.com>
2024-07-16 18:56:06 -0300

    Normal

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { Calendar } from "@nextui-org/calendar";
-import { today, getLocalTimeZone } from "@internationalized/date";
 
 export default function CalendarUI() {
   return (
-    <Calendar
-      aria-label="Date (Max Date Value)"
-      defaultValue={today(getLocalTimeZone())}
-      maxValue={today(getLocalTimeZone())}
-    />
+    <div>
+      <span>Calendario</span>
+    </div>
   );
 }
