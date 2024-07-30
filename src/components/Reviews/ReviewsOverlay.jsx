14660ca1cb6b89b8d9c1865d7f8007f987361380
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2024-07-30 18:43:29 -0300

    fix added to all components + responsivity issues

--- src/components/Reviews/ReviewsOverlay.jsx
+++ src/components/Reviews/ReviewsOverlay.jsx
@@ -97,9 +97,28 @@ const ReviewsOverlay = () => {
           <div className={style.reviewContent}>
             <div className={style.topInfo}>
               <div className={style.topTrackInfo}>
-                <div className={style.nameRelease}>
-                  <span className={style.trackName}>{selectedTrack?.name}</span>
-                  <span className={style.trackReleaseDate}>{releaseDate}</span>
+                <div className={style.trackHeroInfo}>
+                  <div className={style.trackHeroTitle}>
+                    <span className={style.TrackName}>
+                      {selectedTrack?.name}
+                    </span>
+                    {selectedTrack?.explicit && (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="1em"
+                        height="1em"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m-5 6h-3v2h3c.55 0 1 .45 1 1s-.45 1-1 1h-3v2h3c.55 0 1 .45 1 1s-.45 1-1 1h-4c-.55 0-1-.45-1-1V8c0-.55.45-1 1-1h4c.55 0 1 .45 1 1s-.45 1-1 1"
+                        />
+                      </svg>
+                    )}
+                  </div>
+                  <span className={style.heroReleaseYear}>
+                    {selectedTrack.album.release_date.split("-")[0]}
+                  </span>
                 </div>
                 <span className={style.trackArtistName}>
                   {selectedTrack?.album.artists[0].name}
